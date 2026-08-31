351422b9024f7bf94d93ecadf483ec05628b88da
madisongrieger <mgrieger01@saintmarys.edu>
2026-08-31 16:50:20 -0400

    Problem 1 solved

--- problem1.py
+++ problem1.py
@@ -1,5 +1,8 @@
 def rectangle_area(length, width):
-  """
-  ### erase this comment and add your docstring here
-  """
-  ### add your solution here
+    print(length*width)
+length=2
+width=10
+area =rectangle_area(length,width)
+length=2
+width=10
+area =rectangle_area(length,width)
