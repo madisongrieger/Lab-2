2a1a5165126a2093868a1b349adb7d82147b1ed1
madisongrieger <mgrieger01@saintmarys.edu>
2026-09-01 18:33:27 -0400

    solved Problem 4

--- problem4.py
+++ problem4.py
@@ -1,9 +1,17 @@
 def sum_coins(num_pennies, num_nickels, num_dimes, num_quaters):
-  ### write your solution here
+ total_dollars=(num_pennies*.01+ num_nickels*.05+ num_dimes*.10+ num_quaters*.25)
+ return total_dollars
+
+
+
+
+
+
 
 def return_amount(amount_paid, amount_owed):
   """ float, float -> float
   amount_paid and amount_owed are dollar amounts
   Takes in the amount of money paid, subtracts the amount owed, in order to calculate the amount due back to the customer
   """
-  returen ### write your solution here
+  amount_left= amount_paid-amount_owed
+  return amount_left
