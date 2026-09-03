a51a83ad3780439585dc71db5f91903a1c83937b
madisongrieger <mgrieger01@saintmarys.edu>
2026-09-03 09:13:54 -0400

    Solved problem 3

--- problem3.py
+++ problem3.py
@@ -1,13 +1,12 @@
 import math
 def circle_area(radius):
-
    return math.pi * (radius**2)
 
 def square_area(side):
  return side **2
 
 def inscribed_area(side)
- return square_area(side) - circle_area(side/2)
+  return square_area(side) - circle_area(side/2)
 
 
 
