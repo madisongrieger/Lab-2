f42319c44db0535a0cabf7be8acbfccd5a72acd4
madisongrieger <mgrieger01@saintmarys.edu>
2026-09-07 19:57:05 -0400

    Add docstring to 2
    Added docstring to cylinder_volume function.

--- problem2.py
+++ problem2.py
@@ -1,4 +1,7 @@
 def cylinder_volume(radius, height):
     import math
+    """ 
+    takes the radius and height and outputs volume
+    """
     volume= math.pi * (radius**2) * height
     print(volume)
