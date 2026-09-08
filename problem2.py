a7aa4d966ffa82ca086a85703cd291c49a87bf95
madisongrieger <mgrieger01@saintmarys.edu>
2026-09-08 15:12:18 -0400

    Correct typos in cylinder_volume function documentation + added test run used
    Fix typos in docstring examples and update function call.

--- problem2.py
+++ problem2.py
@@ -1,11 +1,13 @@
  import math
 def cylinder_volume(radius, height):
     """ 
+    number -> number
     takes the radius and height and outputs volume
-    >>>cyliner_volume(1,2)
+    >>> cylinder_volume(1,2)
     6.283185307179586
-    >>>cyliner_volume(3,4)
+    >>> cylinder_volume(3,4)
     113.09733552923255
     """
     volume= math.pi * (radius**2) * height
     print(volume)
+cylinder_volume(1,2)
