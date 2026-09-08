1a95b492a79b3f0aab52b10b70646fcc1f854dd1
madisongrieger <mgrieger01@saintmarys.edu>
2026-09-08 14:17:03 -0400

    docstring problem 3

--- problem3.py
+++ problem3.py
@@ -1,13 +1,33 @@
 import math
 def circle_area(radius):
-   return math.pi * (radius**2)
+  """
+  number->number
+  takes the radius and finds the area
+  >>> circle_area(10)
+  314.1592653589793
+  >>> circle_area(4)
+  50.26548245743669 
+  """
+return math.pi * (radius**2)
 
 def square_area(side):
- return side **2
+   """
+   number -> number
+   takes a number and finds the area
+   >>>square_area(3)
+   9
+   >>>square_area(10)
+   100
+   """
+   return side **2
 
-def inscribed_area(side)
-  return square_area(side) - circle_area(side/2)
-
-
-
-  
+def inscribed_area(side):
+   """
+   number -> number
+   takes the areas and subtracts one by the other
+   >>>inscribed_area(12)
+   30.9027
+   >>>inscribed_area(30)
+   193.1417
+   """
+   return square_area(side) - circle_area(side/2)
