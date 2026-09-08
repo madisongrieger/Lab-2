4975ac629d2a397a88451d608e67348b5e99b83e
madisongrieger <mgrieger01@saintmarys.edu>
2026-09-08 15:14:02 -0400

    Correct docstring formatting and add example call
    Fixed formatting of docstring examples and added a call to rectangle_area.

--- problem1.py
+++ problem1.py
@@ -2,9 +2,10 @@ def rectangle_area(length, width):
     """
     number -> number
     takes in a length and a width and prints the area
-    >>>rectangle_area(1,2)
+    >>> rectangle_area(1,2)
     2.0
-    >>>rectangle_area(3,8)
+    >>> rectangle_area(3,8)
     24.0
     """
     print(length*width)
+rectangle_area(1,2)
