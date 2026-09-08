9df10dceb2ecb8cac1d42b8a598507602f37a0cd
madisongrieger <mgrieger01@saintmarys.edu>
2026-09-07 20:26:27 -0400

    docstring problem 2
    Removed redundant import statement and fixed typo in function call.

--- problem2.py
+++ problem2.py
@@ -1,7 +1,11 @@
+ import math
 def cylinder_volume(radius, height):
-    import math
     """ 
     takes the radius and height and outputs volume
+    >>>cyliner_volume(1,2)
+    6.283185307179586
+    >>>cyliner_volume(3,4)
+    113.09733552923255
     """
     volume= math.pi * (radius**2) * height
     print(volume)
