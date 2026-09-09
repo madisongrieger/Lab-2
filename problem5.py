7e042db7f7787b96e5dc38671995e14846f6407a
madisongrieger <mgrieger01@saintmarys.edu>
2026-09-09 19:53:03 -0400

    Add docstring and example to maturity function
    Refactor maturity function to include docstring and example usage.

--- problem5.py
+++ problem5.py
@@ -1,4 +1,13 @@
+from math import log
 def maturity(time, temp, ratio):
-  from math import log
-answer= 23.7 * (time**3) + (temp / 273) + log(ratio)
-print(answer)
+  """
+  number -> number 
+  takes values and gives the maturity
+  >>> maturity(1,2,3)
+  24.798384218386127
+  >>> maturity(2,4,6)
+  191.40590457685655
+  """
+  answer= 23.7 * (time**3) + (temp / 273) + log(ratio)
+  print(answer)
+maturity(1,2,3)
