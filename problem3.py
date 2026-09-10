baa1ea7a54c29d03e4791e79bd23f89ed2835df1
madisongrieger <mgrieger01@saintmarys.edu>
2026-09-10 09:12:08 -0400

    Fix docstrings and formatting in problem3.py
    Updated docstrings for clarity and fixed formatting issues.

--- problem3.py
+++ problem3.py
@@ -3,6 +3,7 @@ def circle_area(radius):
   """
   number->number
   takes the radius and finds the area
+  
   >>> circle_area(10)
   314.1592653589793
   >>> circle_area(4)
@@ -14,9 +15,10 @@ def square_area(side):
    """
    number -> number
    takes a number and finds the area
-   >>>square_area(3)
+   
+   >>> square_area(3)
    9
-   >>>square_area(10)
+   >>> square_area(10)
    100
    """
    return side **2
@@ -25,9 +27,10 @@ def inscribed_area(side):
    """
    number -> number
    takes the areas and subtracts one by the other
-   >>>inscribed_area(12)
+   
+   >>> inscribed_area(12)
    30.9027
-   >>>inscribed_area(30)
+   >>> inscribed_area(30)
    193.1417
    """
    return square_area(side) - circle_area(side/2)
