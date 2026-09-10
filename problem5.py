7a2c4e41423be8f4659c7e63039e905ca7ee7f3e
madisongrieger <mgrieger01@saintmarys.edu>
2026-09-10 13:34:27 -0400

    Fix docstring and output for maturity function
    Updated docstring and corrected output value for maturity function.

--- problem5.py
+++ problem5.py
@@ -1,10 +1,11 @@
 from math import log
 def maturity(time, temp, ratio):
   """
-  number -> number 
+  positive whole number positive whole number positive whole number positive whole number-> number 
   takes values and gives the maturity
+ 
   >>> maturity(1,2,3)
-  24.798384218386127
+  24.805938295994117
   >>> maturity(2,4,6)
   191.40590457685655
   """
