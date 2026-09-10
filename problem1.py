6c333f38bc5d1ef96548be32003fc474c80ce707
madisongrieger <mgrieger01@saintmarys.edu>
2026-09-10 10:36:34 -0400

    Change rectangle_area to return integers
    Updated rectangle_area function to return integer values instead of floats.

--- problem1.py
+++ problem1.py
@@ -3,9 +3,9 @@ def rectangle_area(length, width):
     positive whole number positive whole number -> positive whole number
     takes in a length and a width and prints the area
     >>> rectangle_area(1,2)
-    2.0
+    2
     >>> rectangle_area(3,8)
-    24.0
+    24
     """
     print(length*width)
 rectangle_area(1,2)
