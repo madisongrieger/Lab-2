ef8456b54e3a1c807dc43df1685c740868ec0c32
madisongrieger <mgrieger01@saintmarys.edu>
2026-09-11 12:32:56 -0400

    Enhance BMI function with documentation and examples
    Added docstring to BMI function and example calls.

--- problem6.py
+++ problem6.py
@@ -1,3 +1,14 @@
 def BMI(height, weight):
+    """
+    positive whole number, positive number -> positive number
+    takes in height and weight to calculate BMI
+
+    >>> BMI(68,150)
+    22.80925605536332
+    >>>BMI(75,180)
+    12.497777777777777.
+    """
     Index=(weight/ height**2) * 703
     return Index
+BMI(68,150)    
+    
