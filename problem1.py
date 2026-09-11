005ab993522f80940f370dc4a7f222543ea3d3e9
madisongrieger <mgrieger01@saintmarys.edu>
2026-09-11 12:40:13 -0400

    Update docstring for rectangle_area function
    Fix docstring formatting and add examples for rectangle_area function.

--- problem1.py
+++ problem1.py
@@ -2,6 +2,7 @@ def rectangle_area(length, width):
     """
     positive whole number positive whole number -> positive whole number
     takes in a length and a width and prints the area
+   
     >>> rectangle_area(1,2)
     2
     >>> rectangle_area(3,8)
